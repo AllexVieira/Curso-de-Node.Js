25d67bdafce511e3816006840e42bedbc2e368f3
Alex da Silva Vieira <alexsv2@hotmail.com>
2022-02-08 20:47:40 -0300

    novos testes

---
+++ Test/calc.js
@@ -0,0 +1,22 @@
+function soma(a,b) {
+    return a+b
+}
+
+function sub(a,b) {
+    return a-b
+}
+
+function multi(a,b) {
+    return a*b
+}
+
+function div(a,b) {
+    return a/b
+}
+
+console.log(soma(2,4))
+console.log(sub(2,4))
+console.log(multi(2,4))
+console.log(div(2,4))
+
+console.log("A soma é "+soma(2,4)+", a subtração é "+sub(2,4)+", a multiplicação é "+multi(2,4)+", e a divisão é "+div(2,4)+".")
